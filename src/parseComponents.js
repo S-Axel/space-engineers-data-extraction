07c7993535ef9c7ee0e8ea651b785b4d0e2fef11
S-Axel <sabotieraxel@gmail.com>
2022-10-18 16:51:48 +0200

    feat: add recipe to components, using Blueprints.sbc instead of Components.sbc

--- src/parseComponents.js
+++ src/parseComponents.js
@@ -1,12 +1,28 @@
 import readXmlFile from './readXmlFile.js';
 
+const filterXml = (xml) => (
+  xml.Definitions.Blueprints[0].Blueprint.filter((blueprint) => {
+    return blueprint.Result && blueprint.Result[0].$.TypeId === 'Component';
+  })
+);
+
+const mapXmlToComponents = (xml) => (
+  xml.map((blueprint) => ({
+    typeId: blueprint.Result[0].$.TypeId,
+    subtypeId: blueprint.Result[0].$.SubtypeId,
+    displayName: blueprint.DisplayName[0],
+    recipe: blueprint.Prerequisites[0].Item.map((item) => ({
+      typeId: item.$.TypeId,
+      subtypeId: item.$.SubtypeId,
+      amount: item.$.Amount,
+    })),
+  }))
+);
+
 const parseComponents = async (gameFolder) => {
-  const data = await readXmlFile(`${gameFolder}/Content/Data/Components.sbc`);
-  return data.Definitions.Components[0].Component.map((xmlComponent) => ({
-    typeId: xmlComponent.Id[0].TypeId[0],
-    subtypeId: xmlComponent.Id[0].SubtypeId[0],
-    displayName: xmlComponent.DisplayName[0],
-  }));
+  const xml = await readXmlFile(`${gameFolder}/Content/Data/Blueprints.sbc`);
+  const filteredXml = filterXml(xml);
+  return mapXmlToComponents(filteredXml);
 };
 
 export default parseComponents;
