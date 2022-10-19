08e9d5cfd5fb83a848ec7e3889f72c513740c566
S-Axel <sabotieraxel@gmail.com>
2022-10-19 10:46:30 +0200

    feat: parse all data before writing all files

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ import parseBlocks from './src/parseBlocks.js';
 
 const gameFolder = 'E:/Games/Steam/steamapps/common/SpaceEngineers';
 
-const components = await parseComponents(gameFolder);
-await writeJsonFile('components', components);
-
 const ingots = await parseIngots(gameFolder);
-await writeJsonFile('ingots', ingots);
-
+const components = await parseComponents(gameFolder);
 const blocks = await parseBlocks(gameFolder);
+
+await writeJsonFile('ingots', ingots);
+await writeJsonFile('components', components);
 await writeJsonFile('blocks', blocks);
